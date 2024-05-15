3e8d6febd2305d156531df9e6278d80dce0e85c8
AnGabSS <angelo.gs.santana@outlook.com>
2024-05-15 14:45:09 -0300

    Simple query with query methods

--- src/main/java/com/devsuperior/workshopmongo/service/PostService.java
+++ src/main/java/com/devsuperior/workshopmongo/service/PostService.java
@@ -1,13 +1,12 @@
 package com.devsuperior.workshopmongo.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.workshopmongo.domain.Post;
-import com.devsuperior.workshopmongo.domain.User;
-import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.repository.PostRepository;
 import com.devsuperior.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -20,6 +19,10 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> post = repository.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text){
+		return repository.findByTitleContainingIgnoreCase(text);
 	}
 
 }
